7a425749c906a561427ee9ee6c6a3173a3131d02
wangmingdong <540042281@qq.com>
2019-05-10 18:45:31 +0800

    add、delete、update

--- src/photo/photo.controller.ts
+++ src/photo/photo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Delete, Param, Put, Body } from "@nestjs/common";
 import { Photo } from "./photo.entity";
 import { PhotoService } from "./photo.service";
 
@@ -8,6 +8,21 @@ export class PhotoController {
 
     @Get()
     findAll(): Promise<Photo[]> {
-        return this.photoService.findAll();
+      return this.photoService.findAll();
+    }
+
+    @Post()
+    create(): Promise<any> {
+      return this.photoService.addOne();
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number): Promise<any> {
+        return this.photoService.delete(id);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() photoData: any) {
+        return this.photoService.update(id, photoData);
     }
 }
--- src/photo/photo.service.ts
+++ src/photo/photo.service.ts
@@ -13,4 +13,26 @@ export class PhotoService {
     async findAll(): Promise<Photo[]> {
         return await this.photoRepository.find();
     }
+
+    async addOne(): Promise<Photo> {
+        const newPhoto = new Photo();
+        newPhoto.name = `新增${Math.floor(Math.random() * 1000)}`;
+        newPhoto.description = `描述${Math.floor(Math.random() * 1000)}`;
+        newPhoto.filename = `photo${Math.floor(Math.random() * 1000)}.jpg`;
+        newPhoto.views = 0;
+        newPhoto.isPublished = false;
+        // const savePhoto = await this.photoRepository.save(this.photoRepository.create(newPhoto));
+        const savePhoto = await this.photoRepository.save(newPhoto);
+        return savePhoto;
+    }
+
+    async delete(id: number): Promise<any> {
+        return await this.photoRepository.delete(id);
+    }
+
+    async update(id: number, photo: Photo): Promise<any> {
+        let toUpdate = await this.photoRepository.findOne(id);
+        let updated = Object.assign(toUpdate, photo);
+        return await this.photoRepository.save(updated);
+    }
 }
